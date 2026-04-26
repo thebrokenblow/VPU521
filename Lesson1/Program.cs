2c7f3895152a3d54bd74b7a72fa0c918e7a6cbe8
Артём Красов <artkrasov@gmail.com>
2026-04-26 10:37:04 +0300

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("user32.dll", CharSet = CharSet.Unicode)] 
+static extern int Foo(IntPtr hWnd, string text, string caption, uint type);
+
+var result = Foo(0, "你好世界！", "你好世界！", 0);
+Console.WriteLine(result);  
