c67d5ca2bef7960953714540cf3f6a0553ecd511
Артём Красов <artkrasov@gmail.com>
2026-04-26 13:02:48 +0300

    Второе занятие

---
+++ Lesson2.Example1/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace Lesson2.Example1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private Process? _currentProcess;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        AppDomain domain = AppDomain.CreateDomain("MyDomain");
+    }
+
+    private void CreateProcess_Button_Click(object sender, RoutedEventArgs e)
+    {
+        _currentProcess = Process.Start("D:\\projects\\source\\repos\\VPU521\\Lesson2.Example2\\bin\\Debug\\net10.0-windows\\Lesson2.Example2.exe");
+    }
+
+    private void KillProcess_Button_Click(object sender, RoutedEventArgs e)
+    {
+        _currentProcess?.Kill();
+        _currentProcess = null;
+    }
+}
