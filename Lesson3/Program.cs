e90fce9870d39cb9d4f13889b742fa9304c8a3d0
Артём Красов <artkrasov@gmail.com>
2026-05-10 11:02:37 +0300

    Добавил 4 Занятие без MVVM

---
+++ Lesson3/Program.cs
@@ -0,0 +1,24 @@
+﻿ThreadPool.QueueUserWorkItem(_ => DoWork1());
+ThreadPool.QueueUserWorkItem(_ => DoWork2());
+
+
+Console.ReadLine();
+//8 -> 16
+
+static void DoWork1()
+{
+    for (int i = 0; i < 10; i++)   
+    {
+        Console.WriteLine($"DoWork1");
+        Thread.Sleep(100);
+    }
+}
+
+static void DoWork2()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"DoWork2");
+        Thread.Sleep(100);
+    }
+}
---
+++ Lesson4/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lesson4;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click1(object sender, RoutedEventArgs e)
+    {
+        var value = decimal.Parse(Number1.Text);
+
+        var thread = new Thread(() => CalculateSumNumber(
+            value,
+            isEnabled => Button1.IsEnabled = isEnabled,
+            result => { Result1.Text = result; }));
+
+        thread.Start();
+    }
+
+    private void Button_Click2(object sender, RoutedEventArgs e)
+    {
+        var value = decimal.Parse(Number2.Text);
+
+        var thread = new Thread(() => CalculateSumNumber(
+            value,
+            isEnabled => Button2.IsEnabled = isEnabled,
+            result => { Result2.Text = result; }));
+
+        thread.Start();
+    }
+
+
+    private static void CalculateSumNumber(decimal value, Action<bool> setEnabledButton, Action<string> setResult)
+    {
+        var sum = 0m;
+
+        Application.Current.Dispatcher.BeginInvoke(() => setEnabledButton.Invoke(false));
+
+        for (int i = 1; i < value; i++)
+        {
+            sum += value;
+        }
+
+        Application.Current.Dispatcher.BeginInvoke(() => setEnabledButton.Invoke(true));
+        Application.Current.Dispatcher.BeginInvoke(() => setResult.Invoke(sum.ToString()));
+    }
+}
